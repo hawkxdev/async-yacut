eb9f4db583524c1eb3d98d12099a1114aa23f89b
Sergey Sokolkin <hawkxdev@gmail.com>
2025-12-11 10:55:15 +0300

    feat: add URLForm for short link creation

---
+++ yacut/forms.py
@@ -0,0 +1,18 @@
+"""Формы приложения."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
+
+class URLForm(FlaskForm):
+    """Форма для создания короткой ссылки."""
+
+    original_link = URLField(
+        'Длинная ссылка',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    custom_id = StringField(
+        'Ваш вариант короткой ссылки',
+        validators=[Length(max=16), Optional()]
+    )
+    submit = SubmitField('Создать')
