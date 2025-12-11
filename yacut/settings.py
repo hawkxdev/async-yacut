37413e595a21a323ed870e68f3692aacc1da34bd
Sergey Sokolkin <hawkxdev@gmail.com>
2025-12-11 10:02:10 +0300

    style: fix line length

--- yacut/settings.py
+++ yacut/settings.py
@@ -9,7 +9,11 @@ API_VERSION = 'v1'
 class Config:
     """Настройки Flask-приложения."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI', default='sqlite:///db.sqlite3'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT')
+    SECRET_KEY = os.getenv(
+        'SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT'
+    )
     DISK_TOKEN = os.getenv('DISK_TOKEN', default='')
