6aed395f0e085075bd927f4d4b10c43265d006ea
Sergey Sokolkin <hawkxdev@gmail.com>
2025-12-11 09:33:46 +0300

    feat: initialize flask application

---
+++ yacut/__init__.py
@@ -0,0 +1,14 @@
+"""Инициализация Flask-приложения YaCut."""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from yacut.settings import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from yacut import models
---
+++ yacut/settings.py
@@ -0,0 +1,15 @@
+"""Конфигурация приложения из переменных окружения."""
+import os
+
+
+API_HOST = 'https://cloud-api.yandex.net/'
+API_VERSION = 'v1'
+
+
+class Config:
+    """Настройки Flask-приложения."""
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT')
+    DISK_TOKEN = os.getenv('DISK_TOKEN', default='')
